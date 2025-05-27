0904adef90769a2b96abe84c6def49d4a310cf65
radityaWahyu <radityaw@gmail.com>
2025-05-27 07:51:03 +0700

    update tambah data

--- src/views/DaftarSiswaSample.vue
+++ src/views/DaftarSiswaSample.vue
@@ -3,31 +3,7 @@ import { ref, onMounted, reactive } from 'vue'
 import api from '../services/api'
 
 const items = ref([])
-const isSaved = ref(false)
-
-const formData = reactive({
-  nama: '',
-  alamat: '',
-})
-
 const fetchLoading = ref(false)
-
-const submitForm = async () => {
-  try {
-    isSaved.value = true
-    const response = await api.post('/siswa', formData)
-
-    console.log(response)
-
-    formData.nama = ''
-    formData.alamat = ''
-
-    fetchItems()
-  } catch (error) {
-    console.log(error)
-  }
-  isSaved.value = false
-}
 
 const fetchItems = async () => {
   try {
@@ -43,6 +19,26 @@ const fetchItems = async () => {
   }
 }
 
+const formData = reactive({
+  nama: '',
+  alamat: '',
+})
+
+const submitForm = async () => {
+  try {
+    const response = await api.post('/siswa', formData)
+
+    console.log(response)
+
+    formData.nama = ''
+    formData.alamat = ''
+
+    fetchItems()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 onMounted(() => {
   fetchItems()
 })
@@ -50,37 +46,36 @@ onMounted(() => {
 <template>
   <div class="space-y-4">
     <div>
-      <form class="max-w-sm mx-auto" @submit.prevent="submitForm">
+      <form class="max-w-sm mx-auto" @submit.prevent="submitForm()">
+        <h2 class="text-center text-lg font-semibold py-4">Form Tambah data</h2>
         <div class="mb-5">
-          <label for="nama" class="block mb-2 text-sm font-medium text-black">Nama</label>
+          <label for="nama" class="block mb-2 text-sm font-medium text-gray-900">Nama</label>
           <input
             type="text"
             id="nama"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="masukkan nama siswa..."
-            required
             v-model="formData.nama"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="Inputkan nama"
+            required
           />
         </div>
         <div class="mb-5">
-          <label for="alamat" class="block mb-2 text-sm font-medium text-gray-900">alamat</label>
+          <label for="alamat" class="block mb-2 text-sm font-medium text-gray-900">Alamat</label>
           <input
             type="text"
             id="alamat"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="Masukkan alamat..."
-            required
             v-model="formData.alamat"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="Inputkan alamat..."
+            required
           />
         </div>
 
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          :disabled="isSaved"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          <span v-if="isSaved"> Proses Simpan</span>
-          <span v-else> Simpan</span>
+          Kirim Data
         </button>
       </form>
     </div>
